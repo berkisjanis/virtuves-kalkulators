e717e1c45f85f69f9f081d69616b550719c56b8c
Janis <jb@ited.lv>
2026-06-09 18:30:13 +0300

    Fix: remove Base44 auth wrapper causing blank page

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,10 +1,6 @@
 @echo off
 cd /d "%~dp0"
-git config --global user.email "jb@ited.lv"
-git config --global user.name "Janis"
 git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/berkisjanis/virtuves-kalkulators.git
-git branch -M main
-git push -u origin main
+git commit -m "Fix: remove Base44 auth wrapper causing blank page"
+git push
 pause
